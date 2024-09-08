a030b432cebede70e5816c730a9acc2f36d36f91
Yousuke7052 <130848000+Yousuke7052@users.noreply.github.com>
2024-09-09 01:52:53 +0800

    Update run_test_sh.sh
    Update run_test_sh.sh

--- run_test_sh.sh
+++ run_test_sh.sh
@@ -1,14 +1,25 @@
+# 配置OSS访问密钥等信息
+OSS_ACCESS_KEY_ID= ${OSS_ACCESS_KEY_ID}
+OSS_SECRET_ACCESS_KEY= ${OSS_SECRET_ACCESS_KEY}
+OSS_ENDPOINT= ${OSS_ENDPOINT}
+OSS_BUCKET_NAME= ${OSS_BUCKET_NAME}
+
 # 拉取最新的代码
 git pull
 
-# 创建 .ossutilconfig 文件
-echo "[Default]" > ~/.ossutilconfig
-echo "AccessKeyId = ${OSS_ACCESS_KEY_ID}" >> ~/.ossutilconfig
-echo "AccessKeySecret = ${OSS_SECRET_ACCESS_KEY}" >> ~/.ossutilconfig
-echo "Endpoint = ${OSS_ENDPOINT}" >> ~/.ossutilconfig
+# 检查 .ossutilconfig 文件是否存在，如果不存在则创建
+if [ ! -f ~/.ossutilconfig ]; then
+  cat <<EOT > ~/.ossutilconfig
+[Default]
+OSS_ACCESS_KEY_ID= ${OSS_ACCESS_KEY_ID}
+OSS_SECRET_ACCESS_KEY= ${OSS_SECRET_ACCESS_KEY}
+OSS_ENDPOINT= ${OSS_ENDPOINT}
+OSS_BUCKET_NAME= ${OSS_BUCKET_NAME}
+EOT
+fi
 
 # 给脚本添加执行权限
-chmod +x test_sh.sh
+chmod +x scripts/run_test_sh.sh
 
 # 运行Shell脚本
-./test_sh.sh
+./scripts/run_test_sh.sh
